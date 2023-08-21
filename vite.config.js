17657ce13121613e515a247945e52eddd94b8697
stevenmaharaj <stevesamah@gmail.com>
2023-08-21 17:42:08 +1000

    base f_web

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/f_website/',
+  base: '/f_website/',
   plugins: [
     vue(),
   ],
